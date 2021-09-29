8b064b83cd250acef8347e69aea1c56b9d9ea267
Thanh Nguyen <vnscriptkid@gmail.com>
2021-09-29 16:31:32 +0700

    Hot reload reducers

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,23 +1,28 @@
-import {createStore, applyMiddleware} from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension/developmentOnly";
 
 import thunk from "redux-thunk";
 
 import rootReducer from "../reducers/rootReducer";
 
 export default function configureStore(preloadedState) {
-    const middlewares = [thunk];
-    const middlewareEnhancer = applyMiddleware(...middlewares);
+  const middlewares = [thunk];
+  const middlewareEnhancer = applyMiddleware(...middlewares);
 
-    const storeEnhancers = [middlewareEnhancer];
+  const storeEnhancers = [middlewareEnhancer];
 
-    const composedEnhancer = composeWithDevTools(...storeEnhancers);
+  const composedEnhancer = composeWithDevTools(...storeEnhancers);
 
-    const store = createStore(
-        rootReducer,
-        preloadedState,
-        composedEnhancer
-    );
+  const store = createStore(rootReducer, preloadedState, composedEnhancer);
 
-    return store;
+  if (process.env.NODE_ENV !== "production") {
+    if (module.hot) {
+      module.hot.accept("../reducers/rootReducer", () => {
+        const newRootReducer = require("../reducers/rootReducer").default;
+        store.replaceReducer(newRootReducer);
+      });
+    }
+  }
+
+  return store;
 }
