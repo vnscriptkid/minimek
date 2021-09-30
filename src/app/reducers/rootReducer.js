087a51a7512665e7c56dad2fdf886e68def1e2c7
Thanh Nguyen <vnscriptkid@gmail.com>
2021-09-30 11:38:50 +0700

    Connect UnitInfo with redux

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import tabsReducer from "features/tabs/tabsReducer";
+import unitInfoReducer from "features/unitInfo/unitInfoReducer";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
   tabs: tabsReducer,
+  unitInfo: unitInfoReducer,
 });
 
 export default rootReducer;
--- src/features/unitInfo/UnitInfo.jsx
+++ src/features/unitInfo/UnitInfo.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { connect } from "react-redux";
 import { Form, Dropdown, Segment } from "semantic-ui-react";
+import { selectUnitInfo } from "./unitInfoSelectors";
 
 const FACTIONS = [
   { value: "cc", text: "Capellan Confederation" },
@@ -9,19 +11,26 @@ const FACTIONS = [
   { value: "lc", text: "Lyran Commonwealth" },
 ];
 
-const UnitInfo = () => (
-  <Segment attached="bottom">
-    <Form size="large">
-      <Form.Field name="name" width={6}>
-        <label>Unit Name</label>
-        <input placeholder="Name" />
-      </Form.Field>
-      <Form.Field name="affiliation" width={6}>
-        <label>Affiliation</label>
-        <Dropdown selection options={FACTIONS} />
-      </Form.Field>
-    </Form>
-  </Segment>
-);
+const mapState = (state) => ({
+  unitInfo: selectUnitInfo(state),
+});
 
-export default UnitInfo;
+const UnitInfo = ({ unitInfo = {} }) => {
+  const { name, affiliation } = unitInfo;
+  return (
+    <Segment attached="bottom">
+      <Form size="large">
+        <Form.Field name="name" width={6}>
+          <label>Unit Name</label>
+          <input placeholder="Name" value={name} />
+        </Form.Field>
+        <Form.Field name="affiliation" width={6}>
+          <label>Affiliation</label>
+          <Dropdown selection options={FACTIONS} value={affiliation} />
+        </Form.Field>
+      </Form>
+    </Segment>
+  );
+};
+
+export default connect(mapState)(UnitInfo);
