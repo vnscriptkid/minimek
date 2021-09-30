1090a6ad5b2ef3a2af0d85ec40f5c801314b243b
Thanh Nguyen <vnscriptkid@gmail.com>
2021-09-30 13:42:08 +0700

    Set up entities reducer & redux-orm

--- src/app/reducers/rootReducer.js
+++ src/app/reducers/rootReducer.js
@@ -1,10 +1,12 @@
 import tabsReducer from "features/tabs/tabsReducer";
 import unitInfoReducer from "features/unitInfo/unitInfoReducer";
 import { combineReducers } from "redux";
+import entitiesReducer from "./entitiesReducer";
 
 const rootReducer = combineReducers({
   tabs: tabsReducer,
   unitInfo: unitInfoReducer,
+  entities: entitiesReducer,
 });
 
 export default rootReducer;
