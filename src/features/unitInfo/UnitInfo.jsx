02da1272501544aff9f29d213d293ad6a690b2d1
Thanh Nguyen <vnscriptkid@gmail.com>
2021-10-03 11:03:09 +0700

    Update unit info from ui

--- src/features/unitInfo/UnitInfo.jsx
+++ src/features/unitInfo/UnitInfo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { connect } from "react-redux";
 import { Form, Dropdown, Segment } from "semantic-ui-react";
 import { selectUnitInfo } from "./unitInfoSelectors";
+import { updateUnitInfo } from "./unitInfoActions";
 
 const FACTIONS = [
   { value: "cc", text: "Capellan Confederation" },
@@ -15,22 +16,49 @@ const mapState = (state) => ({
   unitInfo: selectUnitInfo(state),
 });
 
-const UnitInfo = ({ unitInfo = {} }) => {
+const actions = { updateUnitInfo };
+
+const UnitInfo = ({ unitInfo = {}, updateUnitInfo }) => {
   const { name, affiliation } = unitInfo;
+
+  const onAffiliationChange = (e, result) => {
+    const { name, value } = result;
+    const newValues = { [name]: value };
+    updateUnitInfo(newValues);
+  };
+
+  const onNameChange = (e) => {
+    const { name, value } = e.target;
+
+    const newValues = { [name]: value };
+    updateUnitInfo(newValues);
+  };
+
   return (
     <Segment attached="bottom">
       <Form size="large">
         <Form.Field name="name" width={6}>
           <label>Unit Name</label>
-          <input placeholder="Name" value={name} />
+          <input
+            name="name"
+            placeholder="Name"
+            value={name}
+            onChange={onNameChange}
+          />
         </Form.Field>
         <Form.Field name="affiliation" width={6}>
           <label>Affiliation</label>
-          <Dropdown selection options={FACTIONS} value={affiliation} />
+          <Dropdown
+            name="affiliation"
+            selection
+            options={FACTIONS}
+            value={affiliation}
+            onChange={onAffiliationChange}
+          />
         </Form.Field>
       </Form>
     </Segment>
   );
 };
 
-export default connect(mapState)(UnitInfo);
+export default connect(mapState, actions)(UnitInfo);
